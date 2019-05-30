828161af55644bc312ac54cd054d417879835656
HectorBecMe <hecbecme@gmail.com>
2019-05-30 02:12:37 -0500

    Lecture 06. Using Variables

---
+++ MainProject/src/Lecture06.java
@@ -0,0 +1,29 @@
+
+public class Lecture06 {
+
+	public static void main(String[] args) {
+		// Variables
+		int myNumber = 89;
+		short myShort = 847;
+		long myLong = 9797;
+		
+		double myDouble = 7.3243;
+		float myFloat = 345.3f;
+		
+		char myChar = 'A';
+		boolean myBoolean = false;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+
+	}
+
+}
