19e730cba6c5aff70c3955a8e6476b495c0cf1e0
HectorBecMe <hecbecme@gmail.com>
2019-05-30 04:23:38 -0500

    Lecture 08. While loop

---
+++ MainProject/src/Lecture08.java
@@ -0,0 +1,22 @@
+
+public class Lecture08 {
+
+	public static void main(String[] args) {
+
+		// While loop
+		
+		int value = 0;
+		
+		boolean loop = value < 20;
+		
+		while(value < 10)
+		{
+			System.out.println("Hello " + value);
+			//System.out.println(loop);
+			
+			value = value + 1;
+		}
+		
+	}
+
+}
